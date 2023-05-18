98b89d1b5de062267c8b728836335132b033f8b9
alexandre-fernandez <alexandre-fernandez@gmx.com>
2023-05-18 20:00:14 +0200

    foreach

--- pathcrafter/src/core/path/classes/path.class.ts
+++ pathcrafter/src/core/path/classes/path.class.ts
@@ -2,95 +2,15 @@ import Point2d from "@lib/geometry/2d/point2d.class"
 import Vector2d from "@lib/geometry/2d/vector2d.class"
 import PathInternals from "@src/core/path/classes/path-internals.class"
 import NoStartingPoint from "@src/core/path/errors/no-starting-point.error"
-import EmptyVectors from "@src/core/path/errors/empty-vectors.error"
 import StraightVectorProperties from "@src/core/path/classes/straight-vector-properties.class"
 import CubicVectorProperties from "@src/core/path/classes/cubic-vector-properties.class"
 import QuadraticVectorProperties from "@src/core/path/classes/quadratic-vector-properties.class"
 import type { Coordinates2d } from "@lib/geometry/2d/types"
 import type { Coordinates2dGetter, LengthGetter } from "@src/core/path/types"
-/*
-
-movements = [
-	{
-		direction: Vector2.TOP
-		getMovement: () => {}
-	}
-]
-
-*/
+import type { PathInternalsVectorProperties } from "@src/core/path/classes/path-internals.class"
 
 class Path {
 	internals = new PathInternals()
-
-	// #style = {}
-
-	// get #end() {
-	// 	return this.#movements.at(-1)?.().head || null
-	// }
-
-	// get size() {
-	// 	if (!this.#start) throw new NoStartingPoint()
-	// 	return ""
-	// }
-
-	// getParallel(xTranslate: number, yTranslate: number) {
-	// 	const parallel = this.clone()
-	// }
-
-	// get() {
-	// 	if (!this.#start) throw new NoStartingPoint()
-
-	// 	const { x: startX, y: startY } = this.#start()
-	// 	const [firstGetter, ...rest] = this.#movements
-
-	// 	if (!firstGetter) throw new EmptyMovements()
-
-	// 	const first = firstGetter().translate(startX, startY)
-	// 	const path = [first]
-
-	// 	let lastHead = first.head
-	// 	for (const getter of rest) {
-	// 		const nextSegment = getter().translate(lastHead.x, lastHead.y)
-	// 		path.push(nextSegment)
-	// 		lastHead = nextSegment.head
-	// 	}
-
-	// 	return path
-	// }
-
-	toElement() {
-		if (!this.internals.start) throw new NoStartingPoint()
-
-		const startingPoint = this.internals.start()
-		const [first, ...rest] = this.internals.vectorProperties
-
-		if (!first) throw new EmptyVectors()
-
-		const firstTranslated = first
-			.clone()
-			.translate(startingPoint.x, startingPoint.y)
-
-		let d = firstTranslated.toSegment(true)
-
-		let lastHead = firstTranslated.getDisplacement().head
-		for (const vectorProperties of rest) {
-			const nextTranslated = vectorProperties
-				.clone()
-				.translate(lastHead.x, lastHead.y)
-			lastHead = nextTranslated.getDisplacement().head
-			d += ` ${nextTranslated.toSegment()}`
-		}
-
-		const path = document.createElementNS(
-			"http://www.w3.org/2000/svg",
-			"path",
-		)
-		path.setAttribute("d", d)
-		path.setAttribute("fill", "none")
-		path.setAttribute("stroke", "black")
-		path.setAttribute("stroke-width", "2")
-		return path
-	}
 
 	start(startingPoint: Coordinates2d | Coordinates2dGetter | null) {
 		if (startingPoint) {
@@ -200,44 +120,62 @@ class Path {
 		return path
 	}
 
+	toElement() {
+		let d = ""
+
+		this.#forEachTranslatedVectorProperties((vectorProperties, i) => {
+			if (i === 0) {
+				d += vectorProperties.toSegment(true)
+				return
+			}
+			d += ` ${vectorProperties.toSegment()}`
+		})
+
+		const path = document.createElementNS(
+			"http://www.w3.org/2000/svg",
+			"path",
+		)
+		path.setAttribute("d", d)
+		path.setAttribute("fill", "none")
+		path.setAttribute("stroke", "black")
+		path.setAttribute("stroke-width", "2")
+		return path
+	}
+
+	#forEachTranslatedVectorProperties(
+		callback: (
+			vectorProperties: PathInternalsVectorProperties,
+			index: number,
+		) => any,
+	) {
+		if (!this.internals.start) throw new NoStartingPoint()
+
+		const [first, ...vectorProperties] = this.internals.vectorProperties
+		if (!first) return
+
+		const startingPoint = this.internals.start()
+		const translatedFirst = first
+			.clone()
+			.translate(startingPoint.x, startingPoint.y)
+
+		callback(translatedFirst, 0)
+
+		let lastTranslatedHead = translatedFirst.getDisplacement().head
+
+		for (const [i, item] of vectorProperties.entries()) {
+			const translated = item
+				.clone()
+				.translate(lastTranslatedHead.x, lastTranslatedHead.y)
+
+			lastTranslatedHead = translated.getDisplacement().head
+
+			callback(translated, i + 1)
+		}
+	}
+
 	#getterize<T, U extends () => T>(value: Exclude<T, Function> | U): () => T {
 		return typeof value === "function" ? (value as U) : () => value
 	}
-
-	// top(length: number | LengthGetter) {
-	// 	this.#addMovement(this.#toLengthGetter(length), Vector2d.TOP)
-	// 	return this
-	// }
-
-	// right(length: number | LengthGetter) {
-	// 	this.#addMovement(this.#toLengthGetter(length), Vector2d.RIGHT)
-	// 	return this
-	// }
-
-	// bottom(length: number | LengthGetter) {
-	// 	this.#addMovement(this.#toLengthGetter(length), Vector2d.BOTTOM)
-	// 	return this
-	// }
-
-	// left(length: number | LengthGetter) {
-	// 	this.#addMovement(this.#toLengthGetter(length), Vector2d.LEFT)
-	// 	return this
-	// }
-
-	// toString() {
-	// 	return this.get()
-	// 		.map((vector) => vector.toString())
-	// 		.join("\n")
-	// }
-
-	// #addMovement(getLength: LengthGetter, direction: Vector2d) {
-	// 	assertIsCardinalDirection(direction)
-	// 	const clonedDirection = direction.clone()
-	// 	this.#movements.push({
-	// 		direction,
-	// 		get: () => clonedDirection.scalarMultiply(getLength()),
-	// 	})
-	// }
 }
 
 export default Path
