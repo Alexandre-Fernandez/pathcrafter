8156908b732057009c9a380538e797be909f7492
alexandre-fernandez <alexandre-fernandez@gmx.com>
2023-06-09 16:29:45 +0200

    1.0.10

--- pathcrafter/bin/post-publish.cjs
+++ pathcrafter/bin/post-publish.cjs
@@ -5,15 +5,13 @@ const PACKAGE_JSON_DIR = join(__dirname, "..")
 const BACKUP_EXTENSION = ".bk"
 
 const packageJsonPath = join(PACKAGE_JSON_DIR, "package.json")
+
 if (!existsSync(packageJsonPath) || !lstatSync(packageJsonPath).isFile()) {
 	throw new Error("Missing or invalid `package.json`.")
 }
 
 const backupPath = `${packageJsonPath}${BACKUP_EXTENSION}`
-if (!existsSync(backupPath) || !lstatSync(backupPath).isFile()) {
-	throw new Error("Missing or invalid `package.json` backup.")
+if (existsSync(backupPath) && lstatSync(backupPath).isFile()) {
+	copyFileSync(backupPath, packageJsonPath)
+	unlinkSync(backupPath)
 }
-
-copyFileSync(backupPath, packageJsonPath)
-
-unlinkSync(backupPath)
